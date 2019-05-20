bed7a3a204899450210afc29dd208328927ef0c5
LeonardoSarallo <leo.sarallo@gmail.com>
2019-05-20 18:29:16 +0200

    varie

---
+++ footer.php
@@ -0,0 +1,33 @@
+<footer>
+  <div id="footer">
+    <img src="img/gold-enterprise-logo-esteso.svg" class="logo_foot" alt="gold enterprise logo esteso" title="gold enterprise logo esteso" >
+    <div class="contatti_foot">
+      <b>I Contatti di Gold Enterprise</b><br>
+      <ul>
+        <li><a href="mailto:info@goldenterprise.it">info@goldenterprise.it</a></li>
+        <li>055 334 5452634</li>
+        <li>
+          <a href="https://www.facebook.com/goldenterprisefirenze/" target="_blank">
+            <img src="img/social/logo-facebook-white_gold-enterprise.svg" class="social" alt="Collegamento alla pagina Facebook di Gold Enterprise" title="Facebook Gold Enterprise">
+            @goldenterprisefirenze
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/goldenterprisefirenze/" target="_blank">
+            <img src="img/social/logo-instagram-white_gold-enterprise.svg" class="social" alt="Collegamento alla pagina Instagram di Gold Enterprise" title="Instagram Gold Enterprise">
+            @goldenterprisefirenze
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/company/14046322/admin/" target="_blank">
+            <img src="img/social/logo-linkedin-white_gold-enterprise.svg" class="social" alt="Collegamento alla pagina LinkedIn di Gold Enterprise" title="LinkedIn Gold Enterprise">
+            @goldenterprisefirenze
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="info_foot">
+      Gold Enterprise srl<br>via Masaccio 105, Firenze<br>P.I. 06822720485
+    </div>
+  </div>
+</footer>
---
+++ gold-agency.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link
+        href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="bannerimg">
+        <div class="rounded">
+            <a href="http://goldenterprise.it/again2/"><img class="rounded_img" src="img/gold-enterprise-logo.svg" alt=""></a>
+        </div>
+        <img class="b_img" src="img/home/gold-enterprise-agency-banner.jpg" alt="">
+    </div>
+    <div class="bannermobile">
+        <a href="http://goldenterprise.it/again2/">
+            <img src="img/gold-enterprise-logo.svg" alt="">
+        </a>
+    </div>
+    <div class="jumbo_left">
+        <img class="gold_agency" src="img/gold-agency-logo-white.svg" alt="">
+    </div>
+    
+    <div class="jumbo_right">
+        <div class="main_content">
+            
+            <p class="right_text_agency">
+                Explore the<br>
+                <span class="right_text_span">unconventional</span>.
+            </p>
+            <h1 class="maintitle">Gold Agency</h1>
+            <p class="team"><strong>Casa di produzione cinematografica indipendente.</strong>
+                <strong>Storytelling, regia, realtà virtuale, cinema indipendente, documentari, cortometraggi pubblicitari, film e
+                    animazione. <br> </strong>Emozionare, raccontare, far risplendere le storie. Costruiamo lo script, scegliamo il
+                mezzo più adatto per raccontarlo:
+                nessuna sfida ci spaventa.Gold Productions è il volto artistico della famiglia Gold Enterprise, guarda il mondo con
+                occhi attenti e sceglie un taglio autentico e diretto per le sue produzioni audiovisive. <br> Crediamo che le storie
+                debbano trovare una loro voce per essere raccontate, la nostra missione è usare gli strumenti
+                adatti per accorciare ogni distanza</p>
+        </div>      
+        <div class="social_bottom_container">
+            <div class="call_to_agency">
+                <a href="">Scopri il sito</a>
+            </div>
+        </div>
+        <div class="social_page">
+            <a href=""><img class="instagram" src="img/social/logo-instagram_gold-enterprise.svg" alt=""></a>
+            <a href=""><img class="facebook" src="img/loghi/Facebook.svg" alt=""></a>      
+        </div>       
+    </div>
+   <?php include 'footer.php'; ?>
+</body>
+</html>
